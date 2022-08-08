1fab743c3e18ddce841b0c87775c57c40b8aaa07
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-08-08 08:40:43 +0200

    refactor: changed name of parse function

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import { ExchangeRateResult, Rate } from './types';
 import xml2js from 'xml2js';
 import axios from 'axios';
 
-async function parse(string: string): Promise<ExchangeRateResult> {
+async function parseFxrates(string: string): Promise<ExchangeRateResult> {
   const result = await xml2js.parseStringPromise(string);
   const time = result['gesmes:Envelope']['Cube'][0]['Cube'][0]['$']['time'];
 
@@ -28,6 +28,7 @@ export async function fetchFxrates(): Promise<ExchangeRateResult> {
   const result = await axios.get(
     'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'
   );
-  const rates = await parse(result.data);
+  const rates = await parseFxrates(result.data);
   return rates;
 }
+s;
